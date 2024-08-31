73f61e2c93b0e9a3cab84e6951986402584bfbb4
blackcat <97760591+Raabov@users.noreply.github.com>
2024-08-31 15:10:07 -0400

    Added functions to get choices for game to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,42 @@
 console.log(`Start of the game`);
 
-let computerChoice = 0;
-let userChoice = 0;
-
-function getComputerChoice{
+//Function to generate choice for the computer
+function getComputerChoice() {
     
+    let computerChoice = Math.random();
+
+    if (computerChoice > 0.66) {
+        computerChoice = `rock`;
+        console.log(computerChoice);
+    }
+    else if (computerChoice < 0.66 && computerChoice >= 0.33) {
+        computerChoice = `paper`;
+        console.log(computerChoice);
+    }
+
+    else if (computerChoice < 0.33) {
+        computerChoice = `cut`;
+        console.log(computerChoice);
+    }
 }
+
+//Function to prompt user to enter choice
+function getUserChoice() {
+    let userChoice = prompt(`Please choose between rock, paper and cut`);
+    userChoice = userChoice.toLowerCase();
+    console.log(userChoice);
+
+    if (userChoice === `rock`) {
+        userChoice = 0.66;
+        console.log(userChoice);
+    }
+    else if(userChoice === `paper`) {
+        userChoice = 0.33;
+        console.log(userChoice);
+    }
+    else if(userChoice === `cut`)
+        userChoice = 0;
+        console.log(userChoice);
+}
+getComputerChoice();
+getUserChoice();
